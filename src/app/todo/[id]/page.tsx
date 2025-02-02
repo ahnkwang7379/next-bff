a4fa9d2ff895991d54061d5b70e488ffaf4c04ea
ahnkwang7379 <61385865+ahnkwang7379@users.noreply.github.com>
2025-02-02 22:33:04 +0900

    [feat] 기초 페이지 구성 추가

---
+++ src/app/todo/[id]/page.tsx
@@ -0,0 +1,7 @@
+/**
+ * @description
+ * 투두 단일 조회 페이지
+ */
+export default function TodoIdPage() {
+  return <section>TodoIdPage</section>
+}
---
+++ src/app/todo/page.tsx
@@ -0,0 +1,7 @@
+/**
+ * @description
+ * 투두 리스트 보여줄 페이지
+ */
+export default function TodoPage() {
+  return <section>TodoPage</section>
+}
