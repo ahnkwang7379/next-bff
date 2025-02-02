7e1a3afc58c75710485eb72fcd0d25860a0e7424
ahnkwang7379 <61385865+ahnkwang7379@users.noreply.github.com>
2025-02-02 20:18:35 +0900

    [feat] container 생성, 삭제 하는 부분에 gateway 모듈 추가

--- src/libs/di/container.ts
+++ src/libs/di/container.ts
@@ -2,17 +2,20 @@ import { Container } from 'inversify'
 import 'reflect-metadata'
 import DI_SYMBOLS from './types'
 import { IDiReturnTypes } from './returnTypes'
+import gatewayModules from './gateways'
 
 const ApplicationContainer = new Container({
   defaultScope: 'Singleton',
 })
 
 export const initializeContainer = () => {
-  //
+  // GateWays
+  gatewayModules.forEach((module) => ApplicationContainer.load(module))
 }
 
 export const destroyContainer = () => {
-  //
+  // GateWays
+  gatewayModules.forEach((module) => ApplicationContainer.unload(module))
 }
 
 // 의존성 주입을 위한 헬퍼 함수
